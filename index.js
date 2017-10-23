8d1e3ea5895c566bf101e7e63e307a2e923aa519
Austin Kim <ask199416@gmail.com>
2017-10-23 05:21:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -128,8 +128,6 @@ function endGame() {
   })
   window.removeEventListener('keydown', moveDodger)
   document.removeEventListener('keydown', moveDodger)
-  START.innerHTML = 'Play again?'
-  START.style.display = 'inline'
   return alert("YOU LOSE!")
 }
 
