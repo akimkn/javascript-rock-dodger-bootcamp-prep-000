93f4ef34c6191c8e48776470c17fb2d3e56f6d36
Austin Kim <ask199416@gmail.com>
2017-10-23 04:54:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -143,17 +143,17 @@ function endGame() {
 }*/
 
 function moveDodger(e) {
-  if (e.which === LEFT_ARROW || e.which === RIGHT_ARROW) {
-    e.preventDefault()
-    e.preventPropagation()
-  }
   document.addEventListener('keydown', function(e) {
     if (e.which === LEFT_ARROW) {
+      e.preventDefault()
+      e.preventPropagation()
       moveDodgerLeft()
     }
   })
   document.addEventListener('keydown', function(e) {
      if (e.which === RIGHT_ARROW) {
+       e.preventDefault()
+       e.preventPropagation()
       moveDodgerRight()
     }
   })
