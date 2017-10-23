e9caf46b6ef260f36fd298d15f5a3d77a2dd5731
Austin Kim <ask199416@gmail.com>
2017-10-23 04:46:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -124,8 +124,8 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval);
   ROCKS.forEach(function(rock) {rock.remove()})
-  document.removeEventListener("keydown", moveDodger)
-  window.alert("YOU LOSE!")
+  document.removeEventListener('keydown', moveDodger)
+  return alert("YOU LOSE!")
 }
 
 /*function rockSmooth() {
