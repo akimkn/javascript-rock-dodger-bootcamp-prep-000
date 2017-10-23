cc3ba8d5980578f63e61e6664960d4e9df5578c9
Austin Kim <ask199416@gmail.com>
2017-10-23 05:07:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -143,8 +143,7 @@ function endGame() {
 }*/
 
 function moveDodger(e) {
-  document.addEventListener('keydown', function(e) {
-    if (e.which === LEFT_ARROW) {
+  if (e.which === LEFT_ARROW) {
       //e.preventDefault()
       //e.preventPropagation()
       moveDodgerLeft()
